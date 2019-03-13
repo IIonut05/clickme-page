59a0189decb0395d8bbcc971d22b04d494ac0ac4
IIordan <webdevupstart@gmail.com>
2019-03-13 20:33:13 +0200

    adding click me page

---
+++ script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("buton");
+
+// var show = () => {
+// 	alert("ai dat click");
+// }
+
+button.addEventListener("click", function(){
+	alert("ai dat click");
+});
