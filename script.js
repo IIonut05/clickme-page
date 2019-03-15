387f389b4bf1aaa4b3999c28d5166a700c123ac2
IIordan <webdevupstart@gmail.com>
2019-03-15 19:18:13 +0200

    mistake repaired

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var button = document.getElementById("buton");
-var button2 = document.getElementById("buton");
+var button2 = document.getElementById("buton2");
 // var show = () => {
 // 	alert("ai dat click");
 // }
