02210f09989cc5c47cbb9c19417e72e2a415584c
IIordan <webdevupstart@gmail.com>
2019-03-15 19:16:19 +0200

    adding script to button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var button = document.getElementById("buton");
-
+var button2 = document.getElementById("buton");
 // var show = () => {
 // 	alert("ai dat click");
 // }
 
 button.addEventListener("click", function(){
 	alert("ai dat click");
+});
+button2.addEventListener("click", function(){
+	alert("ai dat click a2a oara");
 });
